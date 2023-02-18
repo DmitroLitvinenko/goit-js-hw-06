3c6f8c44162051891e6fbe67785ba4b12138b316
sides1 <dima.litvinenko1990@gmail.com>
2023-02-18 19:59:57 +0200

    complete task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = ingredients.map(item => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.classList.add('item');
+  return ingredient;
+})
+console.log(ingredientsEl);
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.append(...ingredientsEl);
