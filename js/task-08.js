7a876151c2bae3b6fa71231f828144c901529219
sides1 <dima.litvinenko1990@gmail.com>
2023-02-19 19:19:33 +0200

    complete task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
 
+const form = document.querySelector('.login-form');
+const mail = form.elements.email;
+const password = form.elements.password;
+
+console.log(mail, password)
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event){
+    event.preventDefault();
+    if (mail.value === '' || password.value === '') { 
+        alert('Все поля должны быть заполнены!');
+        return;
+};
+
+const formData = { 
+    email: mail.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+
+  form.reset();
+};
+
