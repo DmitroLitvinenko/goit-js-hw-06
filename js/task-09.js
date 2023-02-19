7aa5892d0b391adeaffd21d56e37a3eebb158adb
sides1 <dima.litvinenko1990@gmail.com>
2023-02-19 19:32:04 +0200

    complete task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onAddStyleBodyColor);
+
+function onAddStyleBodyColor () {
+  const body = document.querySelector('body')
+  body.style.backgroundColor = getRandomHexColor();
+
+  const colorValue = document.querySelector('.color')
+  colorValue.textContent = getRandomHexColor();
+};
